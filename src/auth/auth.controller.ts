a6eeb6f28b5d17a35731322519f32880663392d7
Dima <dimaklim04@gmail.com>
2025-04-01 02:28:16 +0300

    add register service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller } from '@nestjs/common'
+
+import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
+
+	public async register() {}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common'
+import { AuthMethod } from '@prisma/__generated__'
+
+import { UserService } from '@/user/user.service'
+
+import { RegisterDto } from './dto/register.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	public constructor(private readonly userService: UserService) {}
+	public async register(dto: RegisterDto) {
+		const isExists = await this.userService.findByEmail(dto.email)
+		if (isExists) {
+			throw new ConflictException(
+				'User with this email already exists. Please use another email or log in.'
+			)
+		}
+		const newUser = await this.userService.create(
+			dto.email,
+			dto.password,
+			dto.name,
+			'',
+			AuthMethod.CREDENTIALS,
+			false
+		)
+		return newUser
+	}
+	public async login() {}
+	public async logout() {}
+	private async saveSession() {}
+}
