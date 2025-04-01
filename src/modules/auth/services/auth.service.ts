5a09055949b49c8f179ccdb7a95cb9d73b87aaf3
Dima <dimaklim04@gmail.com>
2025-04-01 23:55:57 +0300

    add login method and login DTO in auth module

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,14 +1,18 @@
 import {
 	ConflictException,
 	Injectable,
-	InternalServerErrorException
+	InternalServerErrorException,
+	NotFoundException,
+	UnauthorizedException
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { AuthMethod, User } from '@prisma/__generated__'
+import { verify } from 'argon2'
 import { Request, Response } from 'express'
 
 import { UserService } from '@/modules/user/user.service'
 
+import { LoginDto } from '../dto/login.dto'
 import { RegisterDto } from '../dto/register.dto'
 
 @Injectable()
@@ -17,6 +21,7 @@ export class AuthService {
 		private readonly userService: UserService,
 		private readonly configService: ConfigService
 	) {}
+
 	public async register(dto: RegisterDto) {
 		const isExists = await this.userService.findByEmail(dto.email)
 		if (isExists) {
@@ -24,6 +29,7 @@ export class AuthService {
 				'User with this email already exists. Please use another email or log in.'
 			)
 		}
+
 		const newUser = await this.userService.create(
 			dto.email,
 			dto.password,
@@ -34,7 +40,49 @@ export class AuthService {
 		)
 		return newUser
 	}
-	public async login() {}
+
+	public async login(req: Request, dto: LoginDto) {
+		const user = await this.userService.findByEmail(dto.email)
+
+		if (!user || !user.password) {
+			throw new NotFoundException(
+				'User not found. Please check your credentials.'
+			)
+		}
+
+		const isValidPassword = await verify(user.password, dto.password)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException(
+				'Invalid password. Please try again or reset your password if you forgot it.'
+			)
+		}
+
+		// if (!user.isVerified) {
+		//   await this.emailConfirmationService.sendVerificationToken(user.email)
+		//   throw new UnauthorizedException(
+		//     'Your email is not verified. Please check your inbox and confirm your address.'
+		//   )
+		// }
+
+		// if (user.isTwoFactorEnabled) {
+		//   if (!dto.code) {
+		//     await this.twoFactorAuthService.sendTwoFactorToken(user.email)
+		//     return {
+		//       message:
+		//         'Two-factor authentication code required. Please check your email.'
+		//     }
+		//   }
+
+		//   await this.twoFactorAuthService.validateTwoFactorToken(
+		//     user.email,
+		//     dto.code
+		//   )
+		// }
+
+		return this.saveSession(req, user)
+	}
+
 	public async logout(req: Request, res: Response): Promise<void> {
 		return new Promise((resolve, reject) => {
 			req.session.destroy(err => {
@@ -52,6 +100,7 @@ export class AuthService {
 			})
 		})
 	}
+
 	private async saveSession(req: Request, user: User) {
 		return new Promise((resolve, reject) => {
 			req.session.userId = user.id
@@ -60,7 +109,7 @@ export class AuthService {
 				if (err) {
 					return reject(
 						new InternalServerErrorException(
-							'Не удалось сохранить сессию. Проверьте, правильно ли настроены параметры сессии.'
+							'Failed to save session. Please make sure session settings are configured correctly.'
 						)
 					)
 				}
